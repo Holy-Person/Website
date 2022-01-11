dc7a10ce7f376a68307c3709f705515f352da2f2
Holy-Person <44945796+Holy-Person@users.noreply.github.com>
2022-01-11 20:15:07 +0100

    Initial Commit, adding basic design.

---
+++ function.js
@@ -0,0 +1,34 @@
+var cube = document.querySelector('.ivyCube');
+var radioGroup = document.querySelector('.radio-group');
+var currentClass = '';
+
+function changeSide() {
+  var checkedRadio = radioGroup.querySelector(':checked');
+  var showClass = 'show-' + checkedRadio.value;
+  if ( currentClass ) {
+    cube.classList.remove( currentClass );
+  }
+  cube.classList.add( showClass );
+  currentClass = showClass;
+}
+// set initial side
+changeSide();
+
+radioGroup.addEventListener( 'change', changeSide );
+
+var id = null;
+function myMove() {
+  var elem = document.getElementById("myAnimation");
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
